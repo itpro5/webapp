b649f9d9ede2797c7224b8b1a79ea33b4cb16283
Hoang Anh Tuan <hoangtuan1501@gmail.com>
2019-01-16 23:54:21 +0700

    Get GitHub page traffic

--- components/dashboard.js
+++ components/dashboard.js
@@ -4,7 +4,7 @@ const DashboardPage = {
             <v-layout column>
                 <v-flex xs12>
                     <div class="headline">
-                        Your site's stats
+                        Your GitHub Repo's statistics
                     </div>
                 </v-flex>
                 <v-flex xs12>
@@ -13,7 +13,7 @@ const DashboardPage = {
                             <v-flex text-xs-center>
                                 <v-card dark color="primary">
                                     <v-card-text>
-                                        <div class="display-1 font-weight-medium">10000</div>
+                                        <div class="display-1 font-weight-medium">{{repo_views}}</div>
                                         <div class="title font-weight-light">Views</div>
                                     </v-card-text>
                                 </v-card>
@@ -21,7 +21,7 @@ const DashboardPage = {
                             <v-flex text-xs-center>
                                 <v-card dark color="primary">
                                     <v-card-text>
-                                        <div class="display-1 font-weight-medium">1</div>
+                                        <div class="display-1 font-weight-medium">{{repo_unique_visitors}}</div>
                                         <div class="title font-weight-light">Unique Visitors</div>
                                     </v-card-text>
                                 </v-card>
@@ -52,26 +52,31 @@ const DashboardPage = {
     `,
     data: function() {
         return {
-            labels: [
-                '12',
-                '3',
-                '6',
-                '9',
-                '12',
-                '3',
-                '6',
-                '9'
-            ],
-            value: [
-                200,
-                675,
-                410,
-                390,
-                310,
-                460,
-                250,
-                240
-            ]
+            repo_views: null,
+            repo_unique_visitors: null
         }
+    },
+    created: function() {
+        const user_repo_id = localStorage.getItem('github_repo_id')
+        const github_token = localStorage.getItem('github_token')
+        console.log('user_repo_id:', user_repo_id)
+
+        axios.get(`https://api.github.com/repositories/${user_repo_id}/traffic/views`, {
+            headers: {
+                'Authorization': `token ${github_token}`
+            }
+        }).then(
+            response => {
+                console.log('get_repo_traffic() response:', response.data)
+                if (response.data) {
+                    this.repo_views = response.data.count
+                    this.repo_unique_visitors = response.data.uniques
+                }
+            }
+        ).catch(
+            err => {
+                console.log('get_repo_traffic() err:', err)
+            }
+        )
     }
 }
